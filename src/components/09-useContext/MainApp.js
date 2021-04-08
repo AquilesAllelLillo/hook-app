e1e91041c5966cc3c369a856b2c481ee58455b57
AquilesAllelLillo <aquilesallellillo@gmail.com>
2021-04-08 16:24:16 -0400

    useContext

---
+++ src/components/09-useContext/MainApp.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
+
+export const MainApp = () => {
+  const [user, setUser] = useState({});
+
+  return (
+    <div>
+      <h1>MainApp</h1>
+      <hr />
+      <UserContext.Provider value={{ user, setUser }}>
+        <AppRouter />
+      </UserContext.Provider>
+    </div>
+  );
+};
